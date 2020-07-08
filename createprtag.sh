a96338c9e3164f56c0e9a9cda285dd896fa2191f
mdbadiyuzamaevoke <bmohammad@evoketechnologies.com>
2020-07-08 12:54:52 +0530

    Add scripts for creating tags

---
+++ createprtag.sh
@@ -0,0 +1,13 @@
+echo 'Usage => tag=v1.0.0-pr-07012020-1 changes="new features" ./createprtag.sh'
+echo 'Note: Tag will be created from master branch. This is ideal for planned releases with new features or bug fixes.'
+
+: ${tag?"Please specify Tag Name in this format ( vX.X.X-pr-MMDDYYYY-N )"}
+: ${changes?"Please specify features or changes as part of this release"}
+
+echo 'Tag Name: '${tag}
+echo 'Changes: '${changes}
+
+git tag -a ${tag} -m ${changes} origin
+git push origin ${tag}
+
+echo ${tag}' successfully created!'
